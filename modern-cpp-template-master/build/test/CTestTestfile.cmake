4adf88ad0a0bdcfac3ba0c4726f6f5f1864117cd
zongbaoi <2441568697@qq.com>
2021-07-28 05:40:31 -0700

    提交

--- modern-cpp-template-master/build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zongbao/Desktop/modern-cpp-template-master/test
-# Build directory: /home/zongbao/Desktop/modern-cpp-template-master/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ProjectTests "/home/zongbao/Desktop/modern-cpp-template-master/build/test/ProjectTests")
-set_tests_properties(ProjectTests PROPERTIES  _BACKTRACE_TRIPLES "/home/zongbao/Desktop/modern-cpp-template-master/test/CMakeLists.txt;81;add_test;/home/zongbao/Desktop/modern-cpp-template-master/test/CMakeLists.txt;0;")
